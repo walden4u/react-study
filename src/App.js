a97696011b533a59dca715d1f9d388f433e2ebe6
Walden4u <walden4u@gmail.com>
2022-05-11 20:40:15 +0900

    2_4_7

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
+import './App.css';
+
 function App() {
-    const name = '리액트입니다!!!';
-    return (
-        <div
-            style={{
-                backgroundColor: 'black', //background-color -> backgroundColor
-                color: 'aqua',
-                fontSize: '48px', //font-size -> fontSize
-                fontWeight: 'bold', //font-weight -> fontWeight
-                padding: 16 //단위를 생략하면 px로 지정됩니다.
-            }}
-        >
-            {name}
-        </div>
-    );
+    const name = '리액트입니다!';
+    return <div className="react">{name}</div>;
 }
 
 export default App;
