78f4206fc32d3db4265b15ce4a2a945a9ff81c64
Walden4u <walden4u@gmail.com>
2022-05-11 22:36:03 +0900

    3_3_5

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,8 +1,8 @@
-const MyComponent = (props) => {
+const MyComponent = ({ name, children }) => {
     return (
         <>
-            <div>안녕하세요, 제 이름은 {props.name} 입니다.</div>
-            <div>Children 값은 {props.children} 입니다.</div>
+            <div>안녕하세요, 제 이름은 {name} 입니다.</div>
+            <div>Children 값은 {children} 입니다.</div>
         </>
     );
 };
