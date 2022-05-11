33c1eb2a53a471d4f5a4d71824529bd7aabe2a4c
Walden4u <walden4u@gmail.com>
2022-05-11 20:28:59 +0900

    2_4_6

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 function App() {
-    const name = undefined;
-    return <div>{name || '리액트'}</div>;
+    const name = '리액트입니다!!!';
+    const style = {
+        backgroundColor: 'black', //background-color -> backgroundColor
+        color: 'aqua',
+        fontSize: '48px', //font-size -> fontSize
+        fontWeight: 'bold', //font-weight -> fontWeight
+        padding: 16 //단위를 생략하면 px로 지정됩니다.
+    };
+    return <div style={style}>{name}</div>;
 }
 
 export default App;
