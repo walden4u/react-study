78e2af9e699abdeb36803a2c0802fc530a60a30d
Walden4u <walden4u@gmail.com>
2022-05-11 22:11:27 +0900

    3_2_3_2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
 import './App.css';
+import MyComponent from './MyComponent';
 
-function App() {
-    const name = '리액트입니다!';
-    return (
-        <>
-            <div className="react">{name}</div>
-            <input />
-            {/* jsx 주석 입니다. */}
-        </>
-        // js 주석입니다.
-    );
-}
+const App = () => {
+    return <MyComponent />;
+};
 
 export default App;
