ae862d3b739cf5addb1717327d458f52b6028f7b
Walden4u <walden4u@gmail.com>
2022-05-11 19:59:47 +0900

    2_4_1_1

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
+    <>
       <h1>리액트 안녕!</h1>
-      <h2>잘 작동하니?</h2>
-    </div>
+      <h2>잘 작동하니???</h2>
+    </>
   );
 }
