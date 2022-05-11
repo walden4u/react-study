1f008f7ad320590edaf93021b381a5985e5228f7
Walden4u <walden4u@gmail.com>
2022-05-11 22:00:09 +0900

    3_1

---
+++ src/AppClass.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class AppClass extends Component {
+    render() {
+        const name = 'ReactClass';
+        return <div className="react">{name}</div>;
+    }
+}
+
+export default AppClass;
