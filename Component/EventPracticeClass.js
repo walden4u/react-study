618aa3b07f1c04a332de59e0aa76ba41617e3a18
Walden4u <walden4u@gmail.com>
2022-05-15 09:46:15 +0900

    4_2_2

--- Component/EventPracticeClass.js
+++ Component/EventPracticeClass.js
@@ -5,6 +5,14 @@ class EventPracticeClass extends Component {
     return (
       <div>
         <h1>이벤트 연습</h1>
+        <input
+          type="text"
+          name="message"
+          placeholder="아무거나 입려개 보세요"
+          onChange={(e) => {
+            console.log(e);
+          }}
+        />
       </div>
     );
   }
