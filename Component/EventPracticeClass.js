5566cb149a28087cd9959b4644f9d8a687eecb00
Walden4u <walden4u@gmail.com>
2022-05-15 09:58:39 +0900

    4_2_2_3

--- Component/EventPracticeClass.js
+++ Component/EventPracticeClass.js
@@ -1,6 +1,10 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class EventPracticeClass extends Component {
+  state = {
+    message: ''
+  };
+
   render() {
     return (
       <div>
@@ -9,10 +13,20 @@ class EventPracticeClass extends Component {
           type="text"
           name="message"
           placeholder="아무거나 입려개 보세요"
+          value={this.state.message}
           onChange={(e) => {
             console.log(e.target.value);
+            this.setState({ message: e.target.value });
           }}
         />
+        <button
+          onClick={() => {
+            this.setState({ message: '' });
+            alert(this.state.message);
+          }}
+        >
+          초기화
+        </button>
       </div>
     );
   }
