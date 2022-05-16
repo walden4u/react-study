d5ad0113e963a0a0725b5d567f49bfdc39e22db7
Walden4u <walden4u@gmail.com>
2022-05-16 09:37:55 +0900

    6_3

--- Component/IterationSample.js
+++ Component/IterationSample.js
@@ -1,7 +1,7 @@
 const IterationSample = () => {
-    const names = ['눈사람', '얼음', '눈', '바람'];
-    const nameList = names.map((name) => <li>{name}</li>);
-    return <ul>{nameList}</ul>;
+  const names = ['눈사람', '얼음', '눈', '바람'];
+  const nameList = names.map((v, i) => <li key={i}>{v}</li>);
+  return <ul>{nameList}</ul>;
 };
 
 export default IterationSample;
