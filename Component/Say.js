a6fe6686adfc0921df96692dd476c882cad0ef29
Walden4u <walden4u@gmail.com>
2022-05-18 14:49:42 +0900

    3_4_2_3

--- Component/Say.js
+++ Component/Say.js
@@ -2,14 +2,25 @@ import React, { useState } from 'react';
 
 const Say = () => {
   const [message, setMessage] = useState('');
+  const [colorName, setColorName] = useState('');
 
   const onClickIn = () => setMessage('환영합니다!');
   const onClickOut = () => setMessage('안녕히가세요!');
+
   return (
     <>
       <button onClick={onClickIn}>입장</button>
       <button onClick={onClickOut}>퇴장</button>
-      <h1>{message}</h1>
+      <h1 style={{ color: colorName }}>{message}</h1>
+      <button onClick={() => setColorName('red')} style={{ color: 'red' }}>
+        빨간색
+      </button>
+      <button onClick={() => setColorName('green')} style={{ color: 'green' }}>
+        초록색
+      </button>
+      <button onClick={() => setColorName('blue')} style={{ color: 'blue' }}>
+        파란색
+      </button>
     </>
   );
 };
