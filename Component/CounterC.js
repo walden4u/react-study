a52a54a8a7d261183f913c069dc9641bf78ae29d
Walden4u <walden4u@gmail.com>
2022-05-18 10:54:21 +0900

    3_4_1_3

--- Component/CounterC.js
+++ Component/CounterC.js
@@ -6,9 +6,11 @@ class CounterC extends Component {
     fixedNumber: 0
   };
 
-  // onClickCounter = () => {
-  //   this.setState({ number: this.state.number + 1 });
-  // };
+  onClickCounter = () => {
+    this.setState({ number: this.number + 1 });
+    this.setState({ number: this.state.number + 1 });
+    this.setState({ number: this.state.number + 1 });
+  };
 
   render() {
     return (
@@ -16,9 +18,10 @@ class CounterC extends Component {
         <div>바뀌는 값: {this.state.number}</div>
         <div>안바뀌는 값: {this.state.fixedNumber}</div>
         <button
-          onClick={() => {
-            this.setState({ number: this.state.number + 1 });
-          }}
+          onClick={this.onClickCounter}
+          // onClick={() => {
+          //   this.setState({ number: this.state.number + 1 });
+          // }}
         >
           카운터
         </button>
