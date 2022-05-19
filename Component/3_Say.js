80acc0217c9db32eaf5137ceb894f8eaedc626a6
Walden4u <walden4u@gmail.com>
2022-05-19 11:51:01 +0900

    3_say_4

--- Component/3_Say.js
+++ Component/3_Say.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 const Say = () => {
   const [message, setMessage] = useState('');
+  const [color, setColor] = useState('yellow');
 
   const onClickEnter = () => {
     setMessage('환영합니다!!!');
@@ -13,7 +14,20 @@ const Say = () => {
     <>
       <button onClick={onClickEnter}>입장</button>
       <button onClick={onClickExit}>퇴장</button>
-      <div>{message}</div>
+      <br />
+      <div>
+        <h1 style={{ color }}>{message}</h1>
+      </div>
+      <br />
+      <button onClick={() => setColor('red')} style={{ color: 'red' }}>
+        빨강
+      </button>
+      <button onClick={() => setColor('blue')} style={{ color: 'blue' }}>
+        파랑
+      </button>
+      <button onClick={() => setColor('green')} style={{ color: 'green' }}>
+        초록
+      </button>
     </>
   );
 };
