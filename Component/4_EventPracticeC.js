b0ca46370081200005b3759ca8082c563c6f7bf5
Walden4u <walden4u@gmail.com>
2022-05-19 15:36:34 +0900

    4_EventPractice_5

---
+++ Component/4_EventPracticeC.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+
+class EventPracticeC extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: '',
+      password: ''
+    };
+  }
+
+  onChangeInput = (e) => {
+    // console.log(e.target.name);
+    // console.log(e);
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  onClickButton = () => {
+    this.setState({ id: '', password: '' });
+    console.log(this.state.id + ': ' + this.state.password);
+  };
+
+  onKeyInput = (e) => {
+    if (e.key === 'Enter') this.onClickButton();
+  };
+
+  render() {
+    return (
+      <>
+        <h1>이벤트 연습</h1>
+        <input
+          onChange={this.onChangeInput}
+          onKeyPress={this.onKeyInput}
+          value={this.state.id}
+          type="text"
+          name="id"
+          placeholder="id입력해주세요"
+        ></input>
+        <input
+          onChange={this.onChangeInput}
+          onKeyPress={this.onKeyInput}
+          value={this.state.password}
+          type="password"
+          name="password"
+          placeholder="password입력해주세요"
+        ></input>
+        <button onClick={this.onClickButton}>reset</button>
+      </>
+    );
+  }
+}
+
+export default EventPracticeC;
--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import ScrollBox from '../Component/3_SayC';
+import ScrollBox from '../Component/4_EventPracticeC';
 
 const App = () => {
   return (
