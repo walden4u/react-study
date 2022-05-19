0520aee6b7225059290009afd5b4d63a2b2395f4
Walden4u <walden4u@gmail.com>
2022-05-19 10:54:47 +0900

    3_counterC_2

--- Component/3_CounterC.js
+++ Component/3_CounterC.js
@@ -4,7 +4,8 @@ class CounterC extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      counter: 0
+      counter: 0,
+      fixedValue: 0
     };
   }
 
@@ -12,13 +13,18 @@ class CounterC extends Component {
   //   this.setState({ counter: this.state.counter + 1 });
   // }
   onClickPlus = () => {
-    this.setState({ counter: this.state.counter + 1 });
+    //setState에 객체가 아닌 함수로 전달
+    this.setState((prev) => ({ counter: prev.counter + 1 })); //객체값 리턴일 경우 ()필요함
+    this.setState((prev) => {
+      return { counter: prev.counter + 1 };
+    });
   };
 
   render() {
     return (
       <>
         <div>바뀌는 값:{this.state.counter}</div>
+        <div>안바뀌는 값:{this.state.fixedValue}</div>
         <button onClick={this.onClickPlus}>+1</button>
       </>
     );
