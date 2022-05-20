a2c04430f14035de429f35224ef36dddf94b0d26
Walden4u <walden4u@gmail.com>
2022-05-20 10:01:37 +0900

    5_validaiton_2

--- Component/5_Validation.js
+++ Component/5_Validation.js
@@ -2,11 +2,32 @@ import React, { Component } from 'react';
 import './ValidationSample.css';
 
 class Validation extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: '',
+      validate: false
+    };
+  }
+
+  onChangeInput = (e) => {
+    this.setState({ inputValue: e.target.value });
+  };
+  onClickButton = () => {
+    if (this.state.inputValue === '0000') {
+      this.setState({ validate: true });
+    }
+  };
+
   render() {
     return (
       <>
-        <input></input>
-        <button>Validation</button>
+        <input
+          onChange={this.onChangeInput}
+          value={this.state.inputValue}
+          className={this.state.validate ? 'success' : 'failure'}
+        ></input>
+        <button onClick={this.onClickButton}>Validation</button>
       </>
     );
   }
