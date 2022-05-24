bda699052a4d3f81a3b43da4c91c9eff02cbb8ba
Walden4u <walden4u@gmail.com>
2022-05-24 14:11:57 +0900

    8_2_3_cleanup

--- Component/8_Info.js
+++ Component/8_Info.js
@@ -5,7 +5,12 @@ function Info() {
   const [nickname, setNickname] = useState('');
 
   useEffect(() => {
+    console.log('effect');
     console.log(name);
+    return () => {
+      console.log('cleanup');
+      console.log(name);
+    };
   }, [name]);
 
   return (
